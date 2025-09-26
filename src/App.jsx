7c05f46972b6e5ab3d8bacd4b3adf510da819c8e
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-09-26 20:46:36 +0200

    MUI lib + add user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
-import './App.css'
+import {
+  Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  TextField, Button, Paper
+} from '@mui/material'
 
 function App() {
   const [users, setUsers] = useState([])
@@ -15,12 +18,37 @@ function App() {
     setNewUser({ ...newUser, [name]: value })
   }
 
+  const handleAddUser = async () => {
+    if (!newUser.name || !newUser.surname || !newUser.email) return
+
+    try {
+      const res = await fetch(
+        'https://68d45231214be68f8c6902f0.mockapi.io/users',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            ...newUser,
+            age: Number(newUser.age) || 0,
+          }),
+        }
+      )
+
+      if (!res.ok) throw new Error('Failed to create user')
+      const created = await res.json()
+
+      setUsers((prev) => [created, ...prev])
+
+      setNewUser({ name: '', surname: '', age: '', email: '' })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const res = await fetch(
-          'https://68d45231214be68f8c6902f0.mockapi.io/users', {}
-        )
+        const res = await fetch('https://68d45231214be68f8c6902f0.mockapi.io/users')
         const usersData = await res.json()
         setUsers(usersData)
       } catch (e) {
@@ -31,67 +59,74 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <td>name</td>
-            <td>surname</td>
-            <td>age</td>
-            <td>email</td>
-          </tr>
-        </thead>
-        <tbody>
+    <TableContainer component={Paper} sx={{ maxWidth: 900, margin: '20px auto' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell><b>Name</b></TableCell>
+            <TableCell><b>Surname</b></TableCell>
+            <TableCell><b>Age</b></TableCell>
+            <TableCell><b>Email</b></TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {users.map((user) => (
-            <tr>
-              <td>{user.name}</td>
-              <td>{user.surname}</td>
-              <td>{user.age}</td>
-              <td>{user.email}</td>
-            </tr>
+            <TableRow key={user.id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.surname}</TableCell>
+              <TableCell>{user.age}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
           ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>
-              <input
-                type="text"
+          
+          <TableRow>
+            <TableCell>
+              <TextField
+                size="small"
+                label="Name"
                 name="name"
-                onChange={handleChangeNewUser}
                 value={newUser.name}
+                onChange={handleChangeNewUser}
               />
-            </td>
-            <td>
-              <input
-                type="text"
+            </TableCell>
+            <TableCell>
+              <TextField
+                size="small"
+                label="Surname"
                 name="surname"
-                onChange={handleChangeNewUser}
                 value={newUser.surname}
+                onChange={handleChangeNewUser}
               />
-            </td>
-            <td>
-              <input
-                type="text"
+            </TableCell>
+            <TableCell>
+              <TextField
+                size="small"
+                label="Age"
                 name="age"
-                onChange={handleChangeNewUser}
                 value={newUser.age}
-              />
-            </td>
-            <td>
-              <input
-                type="text"
-                name="email"
                 onChange={handleChangeNewUser}
-                value={newUser.email}
               />
-            </td>
-            <td>
-              <button>ADD</button>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+            </TableCell>
+            <TableCell>
+              <TextField
+                size="small"
+                label="Email"
+                name="email"
+                value={newUser.email}
+                onChange={handleChangeNewUser}
+              />
+            </TableCell>
+            <TableCell>
+              <Button variant="contained" onClick={handleAddUser}>
+                ADD
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
